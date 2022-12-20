1ce0618f8baa0619253f872e9a35bebb8b8c91f2
Foze2547 <sorravichsuk@gmail.com>
2022-12-20 16:27:50 +0700

    8.3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,12 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+char before(char x)
+{
+	if (x < 'A' or x > 'Z')
+	{
+		return '0';
+	}
+	else
+	{
+		if (x == 'A')
+		{
+			return 'Z';
+		}
+		else
+		{
+			x = x - 1;
+		}
+	}
+	return x;
 }
 
-int main(){
-	//Test Case
+int main()
+{
+	// Test Case
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
